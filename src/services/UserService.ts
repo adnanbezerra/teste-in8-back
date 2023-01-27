ae76890a1f3bc9f8cd005c90c9a2656be0c37072
seu nome <adnanbezerra@gmail.com>
2023-01-27 19:31:21 -0300

    refactor user service for good coding practices

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,17 +2,18 @@ import jwt from 'jsonwebtoken';
 import { LoginInfo, NewUser } from '../types/UserTypes';
 import bcrypt from 'bcrypt';
 import { conflictError, unauthorizedError } from '../utils/errorUtils';
-import { Users } from '@prisma/client';
 import { userRepository } from '../repositories/UserRepository';
 
 import { SECRET_KEY, EXPIRATION } from '../utils/jwt';
 
 async function createNewAccount(newUser: NewUser): Promise<void> {
-  await checkNewEmailAvailability(newUser);
+  const existingUser = await userRepository.getUserFromDatabase(newUser);
+  if (existingUser) throw conflictError('Email already in use!');
 
+  const hashedPassword = bcrypt.hashSync(newUser.password, 10);
   const payload: NewUser = {
     email: newUser.email,
-    password: bcrypt.hashSync(newUser.password, 10),
+    password: hashedPassword,
     username: newUser.username,
   };
 
@@ -21,7 +22,9 @@ async function createNewAccount(newUser: NewUser): Promise<void> {
 
 async function loginToUser(loginInfo: LoginInfo): Promise<string> {
   const user = await userRepository.getUserFromDatabase(loginInfo);
-  verifyLoginPassword(loginInfo, user);
+  const verify = bcrypt.compareSync(loginInfo.password, user.password);
+
+  if (!verify) throw unauthorizedError('Wrong email or password!');
 
   const payload = {
     id: user.id,
@@ -33,26 +36,6 @@ async function loginToUser(loginInfo: LoginInfo): Promise<string> {
   });
 
   return token;
-}
-
-// auxiliary functions
-
-async function checkNewEmailAvailability(newLogin: NewUser) {
-  const user = await userRepository.getUserFromDatabase(newLogin);
-
-  if (user) throw conflictError('Email already in use!');
-}
-
-function verifyLoginPassword(
-  newLogin: LoginInfo,
-  userFromDatabase: Users,
-): void {
-  const verify = bcrypt.compareSync(
-    newLogin.password,
-    userFromDatabase.password,
-  );
-
-  if (!verify) throw unauthorizedError('Wrong email or password!');
 }
 
 export const userService = {
