8ba343bdded88252aacd834d5558f0c98d7fa751
seu nome <adnanbezerra@gmail.com>
2023-01-27 18:51:23 -0300

    fix new cart item schema

--- src/schemas/NewCartItemSchema.ts
+++ src/schemas/NewCartItemSchema.ts
@@ -1,7 +1,6 @@
 import Joi from 'joi';
 
 export const NewCartItemSchema = Joi.object({
-  userId: Joi.number().required(),
   productId: Joi.number().required(),
   productProvider: Joi.string().allow('brazilian', 'european').required(),
 });
