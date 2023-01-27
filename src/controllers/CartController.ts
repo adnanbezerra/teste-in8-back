09648042e4f625a78f7f1828a854b3c3bec46340
seu nome <adnanbezerra@gmail.com>
2023-01-27 19:16:21 -0300

    fix: route was getting wrong data from req

--- src/controllers/CartController.ts
+++ src/controllers/CartController.ts
@@ -4,7 +4,7 @@ import { cartServices } from '../services/CartServices';
 export async function insertIntoCart(req: Request, res: Response) {
   const payload = {
     productId: req.body.productId,
-    productProvider: req.body.provider,
+    productProvider: req.body.productProvider,
     userId: res.locals.id,
   };
 
