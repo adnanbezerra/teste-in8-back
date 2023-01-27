3736e5bd2c2ae732754ab51a63df494171214a5c
seu nome <adnanbezerra@gmail.com>
2023-01-27 19:46:34 -0300

    refactor: remove unused dotend import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import 'express-async-errors';
-import dotenv from 'dotenv';
 import cors from 'cors';
 import errorHandler from './middlewares/ErrorHandler';
 import { userRouter } from './routes/UserRoute';
 import { cartRouter } from './routes/CartRoutes';
-dotenv.config();
 
 const server = express();
 server.use(cors());
