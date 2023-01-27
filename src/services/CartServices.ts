cc3afe8fff4a92eb927bb200b4419ae840714e9d
seu nome <adnanbezerra@gmail.com>
2023-01-27 19:36:47 -0300

    refactor: services are now classes being exported

--- src/services/CartServices.ts
+++ src/services/CartServices.ts
@@ -1,33 +1,34 @@
 import { cartRepository } from '../repositories/CartRepository';
 import { NewCartTransaction } from '../types/CartTypes';
 
-async function insertIntoCart(payload: NewCartTransaction) {
-  const { productId, productProvider, userId } = payload;
+class CartService {
+  async insertIntoCart(payload: NewCartTransaction) {
+    const { productId, productProvider, userId } = payload;
 
-  const isThereUnboughtCart = await cartRepository.getUnboughtCart(userId);
+    const isThereUnboughtCart = await cartRepository.getUnboughtCart(userId);
 
-  if (isThereUnboughtCart) {
-    await cartRepository.editCart(productId, productProvider, userId);
-  } else {
-    await cartRepository.createUnboughtCart(productId, productProvider, userId);
+    if (isThereUnboughtCart) {
+      await cartRepository.editCart(productId, productProvider, userId);
+    } else {
+      await cartRepository.createUnboughtCart(
+        productId,
+        productProvider,
+        userId,
+      );
+    }
+  }
+
+  async buyCart(userId: number) {
+    await cartRepository.buyCart(userId);
+  }
+
+  async getBoughtCarts(userId: number) {
+    return await cartRepository.getBoughtCarts(userId);
+  }
+
+  async getUnboughtCart(userId: number) {
+    return await cartRepository.getUnboughtCart(userId);
   }
 }
 
-async function buyCart(userId: number) {
-  await cartRepository.buyCart(userId);
-}
-
-async function getBoughtCarts(userId: number) {
-  return await cartRepository.getBoughtCarts(userId);
-}
-
-async function getUnboughtCart(userId: number) {
-  return await cartRepository.getUnboughtCart(userId);
-}
-
-export const cartServices = {
-  insertIntoCart,
-  buyCart,
-  getBoughtCarts,
-  getUnboughtCart,
-};
+export const cartServices = new CartService();
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -6,39 +6,38 @@ import { userRepository } from '../repositories/UserRepository';
 
 import { SECRET_KEY, EXPIRATION } from '../utils/jwt';
 
-async function createNewAccount(newUser: NewUser): Promise<void> {
-  const existingUser = await userRepository.getUserFromDatabase(newUser);
-  if (existingUser) throw conflictError('Email already in use!');
+class UserService {
+  async createNewAccount(newUser: NewUser): Promise<void> {
+    const existingUser = await userRepository.getUserFromDatabase(newUser);
+    if (existingUser) throw conflictError('Email already in use!');
 
-  const hashedPassword = bcrypt.hashSync(newUser.password, 10);
-  const payload: NewUser = {
-    email: newUser.email,
-    password: hashedPassword,
-    username: newUser.username,
-  };
+    const hashedPassword = bcrypt.hashSync(newUser.password, 10);
+    const payload: NewUser = {
+      email: newUser.email,
+      password: hashedPassword,
+      username: newUser.username,
+    };
 
-  await userRepository.createNewAccount(payload);
+    await userRepository.createNewAccount(payload);
+  }
+
+  async loginToUser(loginInfo: LoginInfo): Promise<string> {
+    const user = await userRepository.getUserFromDatabase(loginInfo);
+    const verify = bcrypt.compareSync(loginInfo.password, user.password);
+
+    if (!verify) throw unauthorizedError('Wrong email or password!');
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, {
+      expiresIn: EXPIRATION,
+    });
+
+    return token;
+  }
 }
 
-async function loginToUser(loginInfo: LoginInfo): Promise<string> {
-  const user = await userRepository.getUserFromDatabase(loginInfo);
-  const verify = bcrypt.compareSync(loginInfo.password, user.password);
-
-  if (!verify) throw unauthorizedError('Wrong email or password!');
-
-  const payload = {
-    id: user.id,
-    email: user.email,
-  };
-
-  const token = jwt.sign(payload, SECRET_KEY, {
-    expiresIn: EXPIRATION,
-  });
-
-  return token;
-}
-
-export const userService = {
-  createNewAccount,
-  loginToUser,
-};
+export const userService = new UserService();
